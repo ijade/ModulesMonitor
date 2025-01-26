641d211ded19601d29026b1a3c3bdbc88163d222
Артём Чернышов <inviolablejade@gmail.com>
2025-01-26 05:18:12 +0400

    Fixed sql init scripts

--- Sql/ModuleSensorsInit.sql
+++ Sql/ModuleSensorsInit.sql
@@ -1,14 +1,15 @@
 INSERT INTO public."Sensors"(
 	"PositionIndex", "DecimalPlaces", "MeasuringUnitName", "ModuleId")
-	VALUES (0, 3, 'Degrees Celcius', 1)
+	VALUES (0, 3, 'Degrees Celcius', 1);
 
 INSERT INTO public."Sensors"(
 	"PositionIndex", "DecimalPlaces", "MeasuringUnitName", "ModuleId")
-	VALUES (1, 5, 'ppm', 1)
+	VALUES (1, 5, 'ppm', 1);
 
 INSERT INTO public."Sensors"(
 	"PositionIndex", "DecimalPlaces", "MeasuringUnitName", "ModuleId")
-	VALUES (0, 3, 'g/kg', 2)
+	VALUES (0, 3, 'g/kg', 2);
+	
 INSERT INTO public."Sensors"(
 	"PositionIndex", "DecimalPlaces", "MeasuringUnitName", "ModuleId")
-	VALUES (1, 5, 'ppm', 2)
+	VALUES (1, 5, 'ppm', 2);
--- Sql/ModulesInit.sql
+++ Sql/ModulesInit.sql
@@ -1,19 +1,19 @@
  insert into
- 	public."modules" ("name", "description", "mqtttopic", "createdat")
+ 	public."Modules" ("Name", "Description", "MqttTopic", "CreatedAt")
  values
  	(
  		'firesecurity',
  		'module registering fire security sensor readings',
  		'/firesecuritymodule/values',
  		current_date
- 	)
+ 	);
 
  insert into
- 	public."modules" ("name", "description", "mqtttopic", "createdat")
+ 	public."Modules" ("Name", "Description", "MqttTopic", "CreatedAt")
  values
  	(
  		'climatecontrol',
  		'module registering air condition sensor readings',
  		'/climatecontrolmodule/values',
  		current_timestamp
- 	)
+ 	);

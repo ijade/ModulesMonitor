d216d9a9af0a63c16a95c200f1caaabd27d4d772
Артём Чернышов <inviolablejade@gmail.com>
2025-03-18 23:51:12 +0400

    Added Angular + WebApi SPA from template

---
+++ SPA/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using SPA.Models;
+
+namespace SPA.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+        : base(options, operationalStoreOptions)
+    {
+        
+    }
+}
